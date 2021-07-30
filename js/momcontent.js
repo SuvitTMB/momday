dd0fb451e71889a5be135ece049777782d71fb4f
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-07-30 16:38:01 +0700

    Add files via upload

--- js/momcontent.js
+++ js/momcontent.js
@@ -12,9 +12,15 @@ var qInterval;
 var sGroupChart = "Momday";
 var dateString = "";
 
-var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
-var sLineName = "Website";
-var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
+//var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
+//var sLineName = "Website";
+//var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
+
+var sLineID = sessionStorage.getItem("LineID");
+var sLineName = sessionStorage.getItem("LineName");
+var sLinePicture = sessionStorage.getItem("LinePicture");
+
+
 //sessionStorage.setItem("LineID", sLineID);
 //sessionStorage.setItem("LineName", sLineName);
 //sessionStorage.setItem("LinePicture", sLinePicture);
