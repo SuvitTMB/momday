19254908e6b8db493a34a5bb582bc8427936223d
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-08-01 22:35:54 +0700

    Add files via upload

---
+++ js/sessionID.js
@@ -0,0 +1,17 @@
+var ClearSession = "";
+
+if (typeof(Storage) !== "undefined") {
+  sessionStorage.clear();
+  sessionStorage.setItem("LineID", ClearSession);
+  sessionStorage.setItem("LineName", ClearSession);
+  sessionStorage.setItem("LinePicture", ClearSession);
+  sessionStorage.setItem("LineLogin", ClearSession);
+  sessionStorage.setItem("EmpName", ClearSession);
+  sessionStorage.setItem("EmpID", ClearSession);
+  sessionStorage.setItem("EmpBR", ClearSession);
+} else {
+  alert("กรุณาติดต่อผู้ดูแลระบบ");
+  //document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+
+//sessionStorage.clear();
