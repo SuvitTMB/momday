682e7229ae12ccf934e4aed9664b271d775280fc
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-08-01 22:26:54 +0700

    Add files via upload

--- js/getdata.js
+++ js/getdata.js
@@ -1,11 +1,9 @@
-//var vLineID = "";
+var vLineID = "";
 var vLineName = "";
-//var vLinePicture = "";
-///var vstatusconfirm = 0;
-//document.getElementById('gotopage').style.display='none';
+var vLinePicture = "";
+var vstatusconfirm = 0;
+document.getElementById('gotopage').style.display='none';
 
-
-/*
 var firebaseConfig = {
 	apiKey: "AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE",
 	authDomain: "retailproject-6f4fc.firebaseapp.com",
@@ -17,7 +15,7 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore().collection("CheckProfile");
-*/
+
 
 async function main() {
 	await liff.init({ liffId: "1655966947-rope1zn6" });
@@ -41,36 +39,20 @@ function openWindow() {
 
 async function getUserProfile() {
 	const profile = await liff.getProfile();
-	//vLineID = profile.userId;
+	vLineID = profile.userId;
 	vLineName = profile.displayName;
-	//vLinePicture = profile.pictureUrl;
+	vLinePicture = profile.pictureUrl;
 	sessionStorage.setItem("LineID", profile.userId);
 	sessionStorage.setItem("LineName", profile.displayName);
 	sessionStorage.setItem("LinePicture", profile.pictureUrl);
-	$("#DisplayLineName").html(vLineName);
-	//alert(profile.userId+"---1");
-	//FindID(profile.userId);
-	//CheckLineID(profile.userId);
-    //sessionStorage.setItem("LineID", profile.userId);
+	FindID(profile.userId);
 }
 
-/*
-
-
-//var CheckUserID = "0";
 
 function FindID(gLineID) {
-
-
     db.where('lineID','==',gLineID).get().then((snapshot)=> {
       	snapshot.forEach(doc=> {
-	        //var aStatusConfirm = doc.data().statusconfirm;
-	        //var aStatusPass = doc.data().statuspass;
-	        //var aStatusEdit = doc.data().statusedit;
         	vstatusconfirm = doc.data().statusconfirm;
-
-	        //statusedit == 2 (ล็อคการแก้ไขรหัสและชื่อ-สกุล)
-	        //StatusConfirm == 9 (ล็อคการเข้าใช้โปรแกรม)
 	        if(doc.data().statusconfirm==9) {
 	    		document.getElementById('gotopage').style.display='none';
 		        //alert("กรุณารอการตรวจสอบข้อมูล/ทำการตรวจสอบความถูกต้องของข้อมูลอีกครั้ง");
@@ -81,17 +63,6 @@ function FindID(gLineID) {
 			sessionStorage.setItem("EmpName", doc.data().empName);
 			sessionStorage.setItem("EmpID", doc.data().empID);
 			sessionStorage.setItem("EmpBR", doc.data().empBr);
-	    	//document.getElementById('gotopage').style.display='block';
-			//alert(gLineID+"---2");
-
-			//sessionStorage.setItem("LineLogin", doc.data().statuspass);
-
-			//sessionStorage.setItem("Eid", doc.id);
-			//sessionStorage.setItem("QStatus", doc.data().QStatus);
-			//sessionStorage.setItem("QRatio2", doc.data().QRatio);
-			//sessionStorage.setItem("QDateTime2", doc.data().QDateTime);
-			//$("#DisplayQRatio2").html(doc.data().QRatio);
-			//$("#DisplayQDateTime2").html("เมื่อวันที่ "+doc.data().QDateTime);
 		});
 	});
 
@@ -113,52 +84,6 @@ function FindID(gLineID) {
 	    console.log("Error getting document:", error);
 	});
 }
-
-
-
-function SaveProfile() {
- 	var dateString = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
-    //if(CheckUserID=="1") {
-	//	db.doc(Eid).update({
-    //    lastcheckin : dateString
-    //  });
-    //} else {
-	db.add({
-		lineID : vLineID,
-		linename : vLineName,
-		empPicture : vLinePicture,
-		lastcheckin : dateString
-	});       
-    //}
-}
-
-
-
-function check1(gLineID) {
-	alert("Gid : "+gLineID);
-	db.where('lineID','==',gLineID).get().then((doc) => {
-    if (doc.exists){
-		//alert(doc.exists);
-		//var city = doc.data();
-		console.log(db.toString());
-    	//alert(doc.exists);
-	    alert("มีข้อมูลอยู่แล้ว");
-    } else {
-		alert("ยังไม่มีข้อมูล");
-        console.log("No such document!");
-		SaveProfile();
-    }}).catch((error) => {
-      console.log("Error getting document:", error);
-    });
-}
-
-
-
-	} else {
-	});
-*/
-
-
 
 
 
