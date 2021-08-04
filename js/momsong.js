1d1297bffe83782ef653af51a31e908e99e9e3c0
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-08-04 15:38:30 +0700

    Add files via upload

--- js/momsong.js
+++ js/momsong.js
@@ -15,23 +15,23 @@ function DisplaySong(n) {
   document.getElementById('id01').style.display='block';
   if(n==1) {
     NameSong = '<div class="box-text3">เพลงอิ่ม อุ่น : ศุ บุญ เลี้ยง</div>';
-    NameVDO = '<div><iframe width="290" height="163" src="https://www.youtube.com/embed/jLSUsGtrBKk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    NameVDO = '<div><iframe width="280" height="158" src="https://www.youtube.com/embed/jLSUsGtrBKk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
     NameText = '<div class="YoutubeText">อุ่นใดใด โลกนี้มิมีเทียบเทียม<br>อุ่นอกอ้อมแขน อ้อมกอดแม่ตระกอง<br>รักเจ้าจึงปลูก รักลูกแม่ย่อมห่วงใย<br>ไม่อยากจากไปไกล แม้เพียงครึ่งวัน<br><br>ให้กายเราใกล้กัน ให้ดวงตาใกล้ตา<br>ให้ดวงใจเราสองเชื่อมโยงผูกพัน</div>';
   } else if(n==2) {
     NameSong = '<div class="box-text3">เพลงของขวัญวันแม่ : แดน วรเวช</div>';
-    NameVDO = '<div><iframe width="290" height="163" src="https://www.youtube.com/embed/gU7ghmJ0iCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    NameVDO = '<div><iframe width="280" height="158" src="https://www.youtube.com/embed/gU7ghmJ0iCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
     NameText = '<div class="YoutubeText">เพิ่งรู้ว่าแม่ก้อเหงา ตอนที่ขอให้เราโทรไป<br>เพิ่งรู้ว่าแม่น้อยใจ เมื่อเราอายไม่ยอมให้กอดเรา<br>เพิ่งรู้ว่าเก็บไปคิด ว่าลูกไม่รักเหมือนเก่า<br>เพิ่งรู้ว่าแม่ก็เศร้า เมื่อเราเถียงเมื่อเราขัดใจ</div>';
   } else if(n==3) {
     NameSong = '<div class="box-text3">เพลงคิดถึงพ่อ คิดถึงแม่ คิดถึงบ้าน : น้ององุ่น</div>';
-    NameVDO = '<div><iframe width="290" height="163" src="https://www.youtube.com/embed/WEFOVJwnaVw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    NameVDO = '<div><iframe width="280" height="158" src="https://www.youtube.com/embed/WEFOVJwnaVw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
     NameText = '<div class="YoutubeText">ได้ยินหมอลำ ครวญคร่ำตามสายลมมา<br>สุดแสดเหว่ว้าเมื่อมาพักอยู่เมืองไกล<br>มองสองข้างทาง ตึกสูงบังฟ้าบังใจ<br>อกเอ๋ยอกคนบ้านไพรน้ำตาหลั่งไหลไกลบ้าน</div>';
   } else if(n==4) {
     NameSong = '<div class="box-text3">เพลงจุดธูปบอกแม่ : FLAME</div>';
-    NameVDO = '<div><iframe width="290" height="163" src="https://www.youtube.com/embed/COlNNFakOrw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    NameVDO = '<div><iframe width="280" height="158" src="https://www.youtube.com/embed/COlNNFakOrw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
     NameText = '<div class="YoutubeText">บอกตัวเองให้กลั้นน้ำตาไว้ ไม่ให้ร้อง ไม่ให้น้อยใจ<br>ใกล้ถึงวันแม่ทีไร น้ำตาจะไหลขึ้นมา<br>แม่จากลูกไปตั้งแต่เด็กน้อย มีปมติดตัวคือกำพร้า<br>พยายามเข้มแข็งเรื่อยมา แต่บางเวลาอ่อนล้าเกินไป</div>';
   } else if(n==5) {
     NameSong = '<div class="box-text3">เพลงแม่ : Loso</div>';
-    NameVDO = '<div><iframe width="290" height="163" src="https://www.youtube.com/embed/bimw4o6wNtA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+    NameVDO = '<div><iframe width="280" height="158" src="https://www.youtube.com/embed/bimw4o6wNtA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
     NameText = '<div class="YoutubeText">ป่านนี้ จะเป็นอย่างไร<br>จากมาไกล แสนนาน<br>คิดถึง คิดถึงบ้าน<br>จากมาตั้งนาน เมื่อไรจะได้กลับ</div>';
   } 
   var str = "";
