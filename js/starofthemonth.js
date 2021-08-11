8608b9234986086649e85042da26990e762addf9
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-08-11 10:18:37 +0700

    Add files via upload

--- js/starofthemonth.js
+++ js/starofthemonth.js
@@ -23,7 +23,7 @@ async function getUserProfile() {
   sessionStorage.setItem("LineID", profile.userId);
   sessionStorage.setItem("LineName", profile.displayName);
   sessionStorage.setItem("LinePicture", profile.pictureUrl);
-  alert(profile.userId);
+  //alert(profile.userId);
 }
 
 
